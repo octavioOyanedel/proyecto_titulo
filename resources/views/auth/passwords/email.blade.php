55bd7c0ff0b5490a559d3927a15cc3dffcf15ae5
Octavio Oyanedel <octavio.oyanedel@gmail.com>
2019-12-10 03:51:41 +0000

    Finalizadas plantilla de correo

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header text-center"><h3 class="mb-0">{{ __('Restablecer la contraseña') }}</h3></div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="alert alert-success text-center" role="alert">
+                            <strong>{{ session('status') }}</strong>
                         </div>
                     @endif
 
@@ -18,7 +18,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -34,8 +34,11 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Restablecer la contraseña') }}
                                 </button>
+                                    <a class="btn btn-link" href="{{ route('home') }}">
+                                        {{ __('Ir a login') }}
+                                    </a>
                             </div>
                         </div>
                     </form>
